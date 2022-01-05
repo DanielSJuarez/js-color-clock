9bf6cc7e9be01c95f67d5852f8dce1998f6c65da
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-05 14:09:27 -0500

    constant timer update

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-let currentTime = new Date();
-console.log(currentTime);
+let clockTime = document.querySelector('.clock-Display');
+
+setInterval(function () {
+    // let currentTime = new Date();
+    // let displayTime = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+    // console.log(displayTime);
+}, 1000);
