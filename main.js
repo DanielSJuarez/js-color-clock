5c47bbf7eb27c4be21fdd43138ead8684ff9f23f
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-05 15:15:21 -0500

    padded clock

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-let clockTime = document.querySelector('.clock-Display');
+let clockTime = document.querySelector('.clock-display');
 
-setInterval(function () {
-    // let currentTime = new Date();
-    // let displayTime = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
-    // console.log(displayTime);
+function displayTime() {
+    let getTime = new Date();
+    let seconds = getTime.getSeconds();
+    let minutes = getTime.getMinutes();
+    let hours = getTime.getHours();
+
+    if(seconds < 10){
+        seconds = '0' + seconds;
+    }
+    if(minutes < 10){
+         minutes = '0' + minutes;
+    }
+    if(hours < 10){
+        hours = '0' + hours;
+    }
+    let currentTime = hours + ":" + minutes + ":" + seconds;
+    return currentTime;
+}
+
+setInterval(function() {
+    clockTime.textContent = displayTime();
 }, 1000);
+
+
+
+
+
+
