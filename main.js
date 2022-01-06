700bb0602a10f5d62451d9ab8b214016b7a95121
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-05 21:40:16 -0500

    final commit

--- main.js
+++ main.js
@@ -8,31 +8,32 @@ function displayTimeColor() {
     let minutes = getTime.getMinutes();
     let hours = getTime.getHours();
 
-    if(seconds < 10){
+    if (seconds < 10) {
         seconds = '0' + seconds;
     }
-    if(minutes < 10){
-         minutes = '0' + minutes;
+    if (minutes < 10) {
+        minutes = '0' + minutes;
     }
-    if(hours < 10){
+    if (hours < 10) {
         hours = '0' + hours;
     }
     let currentTime = hours + ":" + minutes + ":" + seconds;
+
     let hexColor = '#' + seconds + hours + minutes;
-    console.log(hexColor);
     clockFaceColor.style.background = hexColor;
+
     clockTime.addEventListener('mouseover', () => {
-        clockTime.textContent =  hexColor;
+        clockTime.textContent = hexColor;
     })
     return currentTime;
 }
 
-function percentTime(){
+function percentTime() {
     let percentDay = new Date();
     let percentSec = percentDay.getSeconds();
     let percentOfMin = percentSec / 60;
-    console.log(percentOfMin);
-    if(percentOfMin == 0){
+    
+    if (percentOfMin == 0) {
         progBar.style.width = '0rem';
     } else {
         let remPercent = percentOfMin * 15;
@@ -40,24 +41,11 @@ function percentTime(){
     }
 }
 
-// function changeBackground(){
-//     let colorTime = new Date();
-//     let colorSec = colorTime.getSeconds();
-//     let colorMin = colorTime.getMinutes();
-//     let colorHrs = colorTime.getHours();
-//     let hexColor = '#' + colorSec + colorHrs + colorMin;
-//     console.log(hexColor);
-//     clockFaceColor.style.background = hexColor;
-// }
-
-setInterval(function() {
+setInterval(function () {
     clockTime.textContent = displayTimeColor();
     percentTime();
-    //changeBackground();
 }, 1000);
 
-// clockTime.addEventListener('mouseover', () => {
-//     clockTime.textContent =  clockTime.background;
-// })
+
 
 
